c1ef706d85709630bb81f6fc5cc476dd2f047c28
= <=>
2020-05-13 19:05:51 +0530

    mongodb example mongo_driver_to_communicate assert

--- node-express/index.js
+++ node-express/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/dishes',dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+  
 
 app.use(express.static(__dirname+ '/public'))
 
