65de63528e494a19b3f4a43173eb7bad92555464
= <=>
2020-05-15 20:18:11 +0530

    passport passport-local hash/salt

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-var FileStore = require('session-file-store')(session)
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -46,29 +48,49 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-function auth (req, res, next) {
-  console.log(req.session);
+//passport auth method
 
-      if(!req.session.user) {
-          var err = new Error('You are not authenticated!');
-          err.status = 403;
-          return next(err);
-      }
-      else {
-        if (req.session.user === 'authenticated') {
-          next();
-        }
-        else {
-          var err = new Error('You are not authenticated!');
-          err.status = 403;
-          return next(err);
-        }
-    }
+function auth (req, res, next) {
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
+  }
 }
+
+
+
+// function auth (req, res, next) {
+//   console.log(req.session);
+
+//       if(!req.session.user) {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 403;
+//           return next(err);
+//       }
+//       else {
+//         if (req.session.user === 'authenticated') {
+//           next();
+//         }
+//         else {
+//           var err = new Error('You are not authenticated!');
+//           err.status = 403;
+//           return next(err);
+//         }
+//     }
+// }
 
 app.use(auth);
 
