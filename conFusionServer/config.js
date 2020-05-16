4f8ff457be7a5307bae07d242722f65b5be4bf42
= <=>
2020-05-16 23:25:39 +0530

    Facebook Oauth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId: '534249367242294',
+        clientSecret: 'b80cc884b1c24eb2ff60a13fda61700e'
+    }
 }
